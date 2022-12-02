5b805d0e43e0a6862e471e576c9ae767b93beab5
robopossum <robo@possum.local>
2022-12-02 08:47:31 +0000

    Day 2

--- run.js
+++ run.js
@@ -9,12 +9,14 @@ console.log('Running Day ' + day + ' Part ' + part);
 const fs = require('fs');
 
 fs.readFile('./days/' + day + '/input', 'utf8', function(err, contents) {
-    var inputs = contents.split('\n');
+  var inputs = contents.split('\n');
+  if (!inputs[inputs.length - 1]) {
     inputs.pop(); //Removes last blank line
+  }
 
-    var script = require('./days/' + day + '/part' + part + '.js');
-    var start = Date.now();
-    var result = script(inputs, contents);
-    var runtime = Date.now() - start;
-    console.log('Got result: ' + result + ' in ' + runtime + ' milliseconds');
+  var script = require('./days/' + day + '/part' + part + '.js');
+  var start = Date.now();
+  var result = script(inputs, contents);
+  var runtime = Date.now() - start;
+  console.log('Got result: ' + result + ' in ' + runtime + ' milliseconds');
 });
