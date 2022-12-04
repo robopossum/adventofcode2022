ace10bb44065781354a954f5d3c861a59eaa9d4e
robopossum <robo@possum.local>
2022-12-04 08:24:17 +0000

    Day 4

---
+++ days/4/part2.js
@@ -0,0 +1,8 @@
+module.exports = function (input, contents) {
+  return input.map(row => row.split(',').map(sect => sect.split('-').map(i => parseInt(i)))).filter(([a, b]) => {
+    return (a[0] >= b[0] && a[0] <= b[1])
+      || (a[1] >= b[0] && a[1] <= b[1])
+      || (b[0] >= a[0] && b[0] <= a[1])
+      || (b[1] >= a[0] && b[1] <= a[1]);
+  }).length;
+};
