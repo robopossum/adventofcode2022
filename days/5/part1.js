eb0ab9243236ee7f2cadb6069f5aedaeb699b4b5
robopossum <robo@possum.local>
2022-12-05 20:30:17 +0000

    Day 5

---
+++ days/5/part1.js
@@ -0,0 +1,22 @@
+module.exports = function (trash, contents) {
+  let [input, rows] = contents.split('\n\n');
+  const stacks = [];
+  input.split('\n').reverse().forEach((str, i) => {
+    if (!i) return;
+    let j = 0;
+    while (j * 4 + 1 < str.length) {
+      stacks[j] = stacks[j] || '';
+      stacks[j] += str[j * 4 + 1].trim();
+      j++;
+    }
+  });
+  rows = rows.split('\n');
+  rows.pop();
+  rows.forEach(command => {
+    let [str, num, from, to] = command.match(/move (\d+) from (\d+) to (\d+)/);
+    let crates = stacks[from - 1].slice(-num);
+    stacks[from - 1] = stacks[from - 1].slice(0, -num);
+    stacks[to - 1] += crates.split('').reverse().join('');
+  });
+  return stacks.reduce((res, str) => res + str.slice(-1), '');
+};
